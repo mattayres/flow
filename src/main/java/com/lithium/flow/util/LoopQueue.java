1dd698e30e6dbed3e6af4207f3c337c489b088fb
Matt Ayres <matt@lithium.com>
2015-11-08 22:28:50 -0800

    FLOW-23 Improved LoopQueue to accept Comparator for priority queue, added finish() method

--- src/main/java/com/lithium/flow/util/LoopQueue.java
+++ src/main/java/com/lithium/flow/util/LoopQueue.java
@@ -18,45 +18,80 @@ package com.lithium.flow.util;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.PriorityBlockingQueue;
 import java.util.function.Consumer;
 
 import javax.annotation.Nonnull;
-
-import com.google.common.collect.Lists;
+import javax.annotation.Nullable;
 
 /**
  * @author Matt Ayres
  */
 public class LoopQueue<T> {
+	private final int capacity;
 	private final BlockingQueue<T> queue;
+	private final List<LoopThread> threads = new CopyOnWriteArrayList<>();
 
+	/**
+	 * @deprecated use {@link #LoopQueue()} and {@link #forList(Consumer)} instead.
+	 */
+	@Deprecated
 	public LoopQueue(@Nonnull Consumer<List<T>> consumer) {
-		this(Integer.MAX_VALUE, consumer);
+		this(Integer.MAX_VALUE);
+		forList(consumer);
 	}
 
+	/**
+	 * @deprecated use {@link #LoopQueue(int)} and {@link #forList(Consumer)} instead.
+	 */
+	@Deprecated
 	public LoopQueue(int capacity, @Nonnull Consumer<List<T>> consumer) {
-		checkNotNull(consumer);
+		this(capacity);
+		forList(consumer);
+	}
 
-		queue = new LinkedBlockingQueue<>(capacity);
+	public LoopQueue() {
+		this(Integer.MAX_VALUE);
+	}
 
-		new LoopThread(10, () -> {
+	public LoopQueue(int capacity) {
+		this(capacity, (Comparator<T>) null);
+	}
+
+	public LoopQueue(int capacity, @Nullable Comparator<T> comparator) {
+		this.capacity = capacity;
+		queue = comparator == null
+				? new LinkedBlockingQueue<T>(capacity)
+				: new PriorityBlockingQueue<>(capacity, comparator);
+	}
+
+	public void forList(@Nonnull Consumer<List<T>> consumer) {
+		threads.add(new LoopThread(10, () -> {
 			if (!queue.isEmpty()) {
-				List<T> list = Lists.newArrayList();
+				List<T> list = new ArrayList<>();
 				queue.drainTo(list);
 				consumer.accept(list);
 			}
-		});
+		}));
+	}
+
+	public void forEach(@Nonnull Consumer<T> consumer) {
+		forList(list -> list.forEach(consumer::accept));
 	}
 
 	public void add(@Nonnull T element) {
 		checkNotNull(element);
-		Sleep.until(() -> queue.offer(element));
+		Sleep.until(() -> queue.size() < capacity && queue.offer(element));
 	}
 
 	public void finish() {
 		Sleep.until(queue::isEmpty);
+		threads.forEach(LoopThread::finish);
 	}
 }
