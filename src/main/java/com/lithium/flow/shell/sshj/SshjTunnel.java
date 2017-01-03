98e11515acb5021cd7668152b229d79ee4233708
Matt Ayres <matt@lithium.com>
2017-01-02 19:27:12 -0800

    FLOW-38 Updated to use sshj library directly for port forwarding

--- src/main/java/com/lithium/flow/shell/sshj/SshjTunnel.java
+++ src/main/java/com/lithium/flow/shell/sshj/SshjTunnel.java
@@ -23,25 +23,16 @@ import com.lithium.flow.shell.Tunneling;
 import com.lithium.flow.util.Logs;
 
 import java.io.IOException;
-import java.net.BindException;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
-import java.net.Socket;
-import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 import javax.annotation.Nonnull;
 
-import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 
-import net.schmizz.concurrent.Event;
-import net.schmizz.sshj.common.LoggerFactory;
-import net.schmizz.sshj.common.SSHPacket;
-import net.schmizz.sshj.common.StreamCopier;
-import net.schmizz.sshj.connection.Connection;
-import net.schmizz.sshj.connection.channel.SocketStreamCopyMonitor;
-import net.schmizz.sshj.connection.channel.direct.AbstractDirectChannel;
+import net.schmizz.sshj.connection.channel.direct.LocalPortForwarder;
+import net.schmizz.sshj.connection.channel.direct.LocalPortForwarder.Parameters;
 
 /**
  * @author Matt Ayres
@@ -49,62 +40,37 @@ import net.schmizz.sshj.connection.channel.direct.AbstractDirectChannel;
 public class SshjTunnel extends Thread implements Tunnel {
 	private static final Logger log = Logs.getLogger();
 
-	private final Tunneling tunneling;
-	private final ServerSocket server;
-	private final Connection connection;
+	private final int port;
+	private final LocalPortForwarder forwarder;
 	private final AtomicBoolean closed = new AtomicBoolean();
 
 	public SshjTunnel(@Nonnull Sshj ssh, @Nonnull Tunneling tunneling) throws IOException {
 		checkNotNull(ssh);
-		this.tunneling = checkNotNull(tunneling);
+		checkNotNull(tunneling);
 
-		server = buildServerSocket(ssh.getRetries());
+		ServerSocket server = new ServerSocket();
 		server.bind(new InetSocketAddress(getHost(), tunneling.getListen()));
+		port = server.getLocalPort();
 
-		connection = ssh.getConnection();
+		Parameters parameters = new Parameters(getHost(), getPort(), tunneling.getHost(), tunneling.getPort());
+		forwarder = ssh.newLocalPortForwarder(parameters, server);
 
 		start();
 	}
 
 	@Override
 	public void run() {
-		log.debug("listening on port {}", server.getLocalSocketAddress());
+		log.debug("tunnel listening on port {}", port);
+
 		while (!closed.get()) {
-			Socket socket = null;
 			try {
-				socket = server.accept();
-				accept(socket);
-			} catch (Exception e) {
-				log.debug("failed to accept connection", e);
-				IOUtils.closeQuietly(socket);
+				forwarder.listen();
+			} catch (IOException e) {
+				log.debug("tunnel failed", e);
 			}
 		}
-		log.debug("tunnel closed");
-	}
 
-	private void accept(@Nonnull Socket socket) throws IOException {
-		log.debug("connection from {}", socket.getRemoteSocketAddress());
-
-		AbstractDirectChannel channel = new AbstractDirectChannel(connection, "direct-tcpip") {
-			@Override
-			@Nonnull
-			protected SSHPacket buildOpenReq() {
-				return super.buildOpenReq()
-						.putString(tunneling.getHost())
-						.putUInt32(tunneling.getPort())
-						.putString(getHost())
-						.putUInt32(server.getLocalPort());
-			}
-		};
-		channel.open();
-
-		socket.setSendBufferSize(channel.getLocalMaxPacketSize());
-		socket.setReceiveBufferSize(channel.getRemoteMaxPacketSize());
-		Event<IOException> soc2chan = new StreamCopier(socket.getInputStream(), channel.getOutputStream(),
-				LoggerFactory.DEFAULT).bufSize(channel.getRemoteMaxPacketSize()).spawnDaemon("soc2chan");
-		Event<IOException> chan2soc = new StreamCopier(channel.getInputStream(), socket.getOutputStream(),
-				LoggerFactory.DEFAULT).bufSize(channel.getLocalMaxPacketSize()).spawnDaemon("chan2soc");
-		SocketStreamCopyMonitor.monitor(5, TimeUnit.SECONDS, soc2chan, chan2soc, channel, socket);
+		log.debug("tunnel closed on port {}", port);
 	}
 
 	@Override
@@ -115,26 +81,12 @@ public class SshjTunnel extends Thread implements Tunnel {
 
 	@Override
 	public int getPort() {
-		return server.getLocalPort();
+		return port;
 	}
 
 	@Override
 	public void close() throws IOException {
 		closed.set(true);
-		server.close();
-	}
-
-	@Nonnull
-	private ServerSocket buildServerSocket(int tries) throws IOException {
-		int count = 0;
-		while (true) {
-			try {
-				return new ServerSocket();
-			} catch (BindException e) {
-				if (++count == tries) {
-					throw e;
-				}
-			}
-		}
+		forwarder.close();
 	}
 }
